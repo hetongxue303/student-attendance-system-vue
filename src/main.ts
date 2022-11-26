5f8149babafbbd3f3a0705db6bbcffa97680e170
hetongxue303 <1974597686@qq.com>
2022-11-27 01:17:59 +0800

    feat: 添加svg图标插件

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,19 @@ import pinia from './store'
 import router from './router'
 import ElementPlus from './plugins/element-plus'
 
-import '@assets/styles/index.scss'
-import 'virtual:windi.css'
 import 'animate.css'
+import 'virtual:windi.css'
+import '@assets/styles/index.scss'
 
 import '@/permission'
+
+import 'virtual:svg-icons-register'
+import SvgIcon from '@components/SvgIcon/Index.vue'
 
 const app = createApp(App)
 
 app.use(pinia).use(router).use(ElementPlus)
+
+app.component('SvgIcon', SvgIcon)
 
 app.mount('#app')
